dbe5e29384c5df1a705cffc2bc64c50b10cdc3f5
Agrima Saraf <agrimasaraf.runu@gmail.com>
2026-08-28 06:33:45 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -19,6 +19,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:3000"],
+    allow_origin_regex=r"https://.*\.vercel\.app",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
